0df036dd4355310025e067857f45adeb44eccda2
saksham0751 <saksham.ashtp@gmail.com>
2019-04-10 10:59:50 +0530

    changed url in setup.py file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	name='es_autocomplete',
 	version='0.1',
 	description='',
-	url='https://github.com/saksham0751/myproject',
+	url='https://github.com/saksham0751/mypackage',
 	licence='MIT',
 	packages=['es_autocomplete'],
 	install_requires=['elasticsearch'],
